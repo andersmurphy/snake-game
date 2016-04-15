5045bbc614524084975e2d60cee6a6ebab080440
Anders <anders.murphy@kotikan.com>
2016-04-15 16:36:32 +0100

    snake now grows when eating apples

---
+++ core/src/main/java/com/andersmurphy/snake/BodyPart.java
@@ -0,0 +1,23 @@
+package com.andersmurphy.snake;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+
+import java.awt.*;
+
+/**
+ * Created by anders on 15/04/2016.
+ */
+public final class BodyPart {
+	public Point bodyPartPosition;
+	public final Texture texture;
+
+	public BodyPart(Texture texture, Point snakePosition) {
+		this.texture = texture;
+		this.bodyPartPosition = snakePosition;
+	}
+
+	public void updateBodyPosition(Point bodyPartPosition) {
+		this.bodyPartPosition = bodyPartPosition;
+	}
+}
--- core/src/main/java/com/andersmurphy/snake/GameScreen.java
+++ core/src/main/java/com/andersmurphy/snake/GameScreen.java
@@ -7,6 +7,8 @@ import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.Array;
 
 import java.awt.*;
 import java.util.HashMap;
@@ -19,12 +21,6 @@ import java.util.function.Function;
 public class GameScreen extends ScreenAdapter {
 	private static final float MOVE_TIME = 0.5F;
 	private static final int SNAKE_MOVEMENT = 32;
-
-	private Point snakePosition = new Point(0, 0);
-	private String snakeDirection = Directions.RIGHT;
-	private float timer = MOVE_TIME;
-	private Batch batch;
-	private Texture snakeHead;
 
 	private static final Map<String, Function<Point, Point>> movement = new HashMap<>();
 
@@ -39,10 +35,23 @@ public class GameScreen extends ScreenAdapter {
 				point -> point.x <= 0 ? new Point(Gdx.graphics.getWidth(), point.y) : new Point(point.x - SNAKE_MOVEMENT, point.y));
 	}
 
+	private Point snakePosition = new Point(0, 0);
+	private String snakeDirection = Directions.RIGHT;
+	private float timer = MOVE_TIME;
+	private Batch batch;
+	private Texture snakeHead;
+	private Texture snakeBody;
+	private Texture apple;
+	private boolean isAppleAvailable = false;
+	private Point applePosition = new Point(0, 0);
+	private Array<BodyPart> bodyParts = new Array<>();
+
 	@Override
 	public void show() {
 		batch = new SpriteBatch();
 		snakeHead = new Texture(Gdx.files.internal("snakehead.png"));
+		snakeBody = new Texture(Gdx.files.internal("snakebody.png"));
+		apple = new Texture(Gdx.files.internal("apple.png"));
 	}
 
 	@Override
@@ -51,15 +60,54 @@ public class GameScreen extends ScreenAdapter {
 		timer -= delta;
 		if (timer <= 0) {
 			timer = MOVE_TIME;
+			Point snakesOldPosition = snakePosition;
 			snakePosition = movement.get(snakeDirection).apply(snakePosition);
+			updateBodyPartsPosition(snakesOldPosition);
+			if (isAppleAvailable && snakePosition.equals(applePosition)) {
+				bodyParts.insert(0, new BodyPart(snakeBody, snakePosition));
+				isAppleAvailable = false;
+			}
 		}
+		checkAndPlaceApple();
+		clearScreen();
+		draw();
+	}
 
-		Gdx.gl.glClearColor(Color.BLACK.r, Color.BLACK.g, Color.BLACK.b, Color.BLACK.a);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
+	private void draw() {
 		batch.begin();
 		batch.draw(snakeHead, snakePosition.x, snakePosition.y);
+		for (BodyPart bodyPart : bodyParts) {
+			if (!snakePosition.equals(bodyPart.bodyPartPosition)) {
+				batch.draw(bodyPart.texture, bodyPart.bodyPartPosition.x, bodyPart.bodyPartPosition.y);
+			}
+		}
+		if (isAppleAvailable) {
+			batch.draw(apple, applePosition.x, applePosition.y);
+		}
 		batch.end();
+	}
+
+	private void clearScreen() {
+		Gdx.gl.glClearColor(Color.BLACK.r, Color.BLACK.g, Color.BLACK.b, Color.BLACK.a);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	}
+
+	private void checkAndPlaceApple() {
+		if (!isAppleAvailable) {
+			do {
+				applePosition.x = MathUtils.random(Gdx.graphics.getWidth() / SNAKE_MOVEMENT - 1) * SNAKE_MOVEMENT;
+				applePosition.y = MathUtils.random(Gdx.graphics.getHeight() / SNAKE_MOVEMENT - 1) * SNAKE_MOVEMENT;
+				isAppleAvailable = true;
+			} while (snakePosition.equals(applePosition));
+		}
+	}
+
+	private void updateBodyPartsPosition(Point snakePosition) {
+		if (bodyParts.size > 0) {
+			BodyPart bodyPart = bodyParts.removeIndex(0);
+			bodyPart.updateBodyPosition(snakePosition);
+			bodyParts.add(bodyPart);
+		}
 	}
 
 	private void queryInput() {
